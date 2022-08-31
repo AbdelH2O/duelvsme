29e020714d407164686f81d2fa6d69f6fde44882
Hana <a.hana@aui.ma>
2022-08-31 16:09:06 +0100

    going back to netlify?

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,9 @@ const config = {
     }),
 
 	kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            split: true,
+        }),
         paths: {
             base: process.env.VITE_NODE_ENV === 'development' ? '' : ''
         },
