fc377082897a89072caad06caa498d146c0cfbe7
Hana <a.hana@aui.ma>
2022-08-31 19:08:01 +0100

    redis connection??

--- src/lib/utils/redisClient.ts
+++ src/lib/utils/redisClient.ts
@@ -1,8 +1,10 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    url: process.env.VITE_REDIS_FULL_URL,
+    url: import.meta.env.VITE_REDIS_FULL_URL,
 });
+// console.log(import.meta.env.VITE_REDIS_FULL_URL);
+
 // client.connect();
 // client.on('error', (err) => console.log('Redis Client Error', err));
 
