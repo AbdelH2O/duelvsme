3aae742ef680fac268a76318ad841f59963bc6e5
Hana <a.hana@aui.ma>
2022-09-04 16:24:46 +0100

    added crypto to matchmake.js

--- worker/src/matchmake.js
+++ worker/src/matchmake.js
@@ -1,6 +1,7 @@
 import client from './redisClient.js';
 import supabase from './utilitySupabase.js';
 import Cf from 'cf-wrapper';
+import crypto from "crypto";
 
 function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
